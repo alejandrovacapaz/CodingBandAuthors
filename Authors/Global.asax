5c4aa39cb1e047d823ff03c6f704dbdd2503398b
Alejandro Vaca <alejadro.vaca@onetree.com>
2018-12-14 10:59:42 -0400

    Initial Commit

---
+++ Authors/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Authors.MvcApplication" Language="C#" %>
---
+++ Authors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Authors.Startup))]
+namespace Authors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
